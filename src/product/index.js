3b94a6ae8489dcb9777a05083a6f907997ddeebf
Hoon-Neumann <ben3157@naver.com>
2022-01-08 18:42:15 +0900

    1. implement product details page  2. apply basic antd

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,43 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProduct] = useState[null];
+  useEffect(function () {
+    axios
+      .get(
+        `https://4ffc4d58-e9c2-41cc-be11-7f0696ce2819.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageurl} />
+      </div>
+      <div id="profile-box">
+        <img src={"/images/icons/avatar.png"} />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
